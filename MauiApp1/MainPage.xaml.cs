71c2b54eee22d0ab52525b6a11042c5622132a1f
Rhys18751996 <18751996@student.westernsydney.edu.au>
2025-01-13 15:38:55 +1100

    complete pages demo now begining layouts demo

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -5,10 +5,24 @@
         public MainPage()
         {
             InitializeComponent();
-        }
+
+
+			LayoutsDemoBtn.Clicked += (sender, args) =>
+			{
+				// Navigate to the next page when the button is clicked
+				Navigation.PushAsync(new ContentPageDemo());
+			};
+		}
+
 
         private async void PagesDemo_Clicked(object sender, EventArgs e)
         {
+			var navigationPage = new NavigationPage(new PagesDemo());
+			await Navigation.PushAsync(navigationPage);
+		}
+
+		private async void LayoutsDemo_Clicked(object sender, EventArgs e)
+		{
 			var navigationPage = new NavigationPage(new PagesDemo());
 			await Navigation.PushAsync(navigationPage);
 		}
---
+++ MauiApp1/Views/LayoutsDemo/StackLayoutPageDemo.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1.Views.LayoutsDemo;
+
+public partial class StackLayoutPageDemo : ContentPage
+{
+	public StackLayoutPageDemo()
+	{
+		InitializeComponent();
+	}
+}
